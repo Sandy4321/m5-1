1c84b4ec95b38698c3e5ec1847e56f5dc9dc518d
Evan <etbrand@gmail.com>
2020-06-30 13:33:54 -0700

    add model stacker training script

---
+++ src/models/train_stacker.py
@@ -0,0 +1,69 @@
+import os
+import random
+from joblib import dump
+import numpy as np
+import pandas as pd
+import calendar as cal
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.metrics import mean_squared_error as MSE
+import lightgbm as lgb
+
+from src.features.build_train_features import *
+from src.features.build_test_features import *
+from src.models.preprocess import *
+from src.models.model_mgmt import *
+
+calendar_path = os.path.join('data', 'calendar.csv')
+sales_path = os.path.join('data', 'sales_train_evaluation.csv')
+price_path = os.path.join('data', 'sell_prices.csv')
+sample_path = os.path.join('data', 'sample_submission.csv')
+
+calendar = pd.read_csv(calendar_path, parse_dates=['date'])
+sales = pd.read_csv(sales_path)
+prices = pd.read_csv(price_path)
+sample = pd.read_csv(sample_path)
+
+ar_lags = {'days':[7]}
+
+cat_features = ['weekday', 'month', 'year',  'store_id',
+                'cat_id', 'state_id', 'event_name_1', 'event_name_2']
+quant_features = ['days', 'sell_price', 'mean']
+str_features = ['store_id', 'cat_id', 'state_id',
+                'event_name_1', 'event_name_2', 'weekday']
+
+ma_periods = [30]
+other_features = ['days', 'weekday', 'sell_price'] #, # 'store_id',
+                 # 'event_name_1', 'event_name_2']
+arma_prefixes = ['ma_', 'days_', 'months_', 'years_']
+
+train_dates = calendar.date[:-28].values
+test_dates = calendar.date[-28:].values
+
+train_features = build_train_features(
+        train_dates=train_dates, ar_lags=ar_lags, ma_periods=ma_periods,
+        other_features=other_features, calendar=calendar,
+        sales=sales, prices=prices)
+arma_features = [col for col in train_features.columns
+                 if any([ap in col for ap in arma_prefixes])]
+feature_names = other_features + arma_features
+cat_names = [fn for fn in feature_names if fn in cat_features]
+quant_names = arma_features + [fn for fn in feature_names
+                               if fn in quant_features]
+str_names = [fn for fn in feature_names if fn in str_features]
+
+cat_df, les = le_cat_features(train_features, str_names)
+
+X_train = pd.concat([train_features[quant_names].reset_index(drop=True),
+                        cat_df], axis=1)
+
+model_path = 'models/model_9128.joblib'
+with open(model_path, 'rb') as f:
+    reg = load(f)
+
+lgbm_preds = reg.predict(X_train)
+
+lgbm_pred_path = 'models/model_9128_preds.joblib'
+with open(lgbm_pred_path, 'wb') as f:
+    dump(lgbm_preds, f)
+
+y_train = train_features.value
